9f38715282bf98e877865bd022e8d1304550e904
Ariann Decker <adecker1207@gmail.com>
2024-04-29 21:29:51 -0400

    removed unused methods

--- weather.py
+++ weather.py
@@ -77,52 +77,5 @@ def get_three_day_forecast(weather):
     
     return forecast
 
-def get_zip(zipcode):
-    g_url = f"{geo_url}appid={api_key}&zip={zipcode},us"
-    r = requests.get(g_url)
-    if r.status_code == 200:
-        data = r.json()
-        lat = data['lat']
-        lon = data['lon']
-        return data['lat'], data['lon']
-    else:
-        print(f"Error: {r.status_code}" + "\n" + "Please try again")
-
-    url = f"{base_url}lat={lat}&lon={lon}&appid={api_key}&units=imperial"
-    res = requests.get(url)
-    data = res.json()
-
-    if r.status_code == 200:
-        return data
-    else:
-        print(f"Error: {r.status_code}" + "\n" + "Please try again")
-
-def get_weather(data):
-    for i in range(3):
-    # Date
-        date_str = data['list'][i]['dt_txt']
-        date = dt.datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
-        date = date.strftime('%m/%d/%Y')
-   
-   # Fetching weather display
-         # Temperature
-        Temp = data['list'][i]['main']['temp']
-          # Humidity
-        humidity = data['list'][i]['main']['humidity']
-           # Description
-        description = data['list'][i]['weather'][0]['description']
-           # Precipitation
-        precipitation = data['list'][i]['pop']
-          # min and max temps
-        min_temp = data['list'][i]['main']['temp_min']
-        max_temp = data['list'][i]['main']['temp_max']
-
-        print(f"Date: {date}\n")
-        print(f"Temperature: {Temp} F\n")
-        print(f"Min: {min_temp}F " + f"Max: {max_temp}F\n")
-        print(f"Humidity: {humidity}%\n")
-        print(f"Precipitation: {precipitation}%\n")
-        print(f"Description: {description}\n")
-
 if __name__ == "__main__":
     main()
